80a835be9e27865e85259624fc2ed7ece3d292e2
pabloapausa <pabloapausa@outlook.com>
2021-11-28 18:30:12 +0100

    Type the project

---
+++ jest.tconfig.js
@@ -0,0 +1,31 @@
+// Template from https://nextjs.org/docs/testing. 
+
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules).
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules).
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // Handle image imports.
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/', '<rootDir>/.next/'
+  ],
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset.
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  testEnvironment: 'jsdom',
+  }
